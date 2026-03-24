60b4e8eeb9be9a6afffbdc40061a1507c772d316
Gabriel Gurgel <gabrielsilvagurgel@gmail.com>
2026-03-23 22:25:28 -0300

    Modifica layout da header e adiciona logo e icones

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,15 +1,24 @@
+<script setup>
+import logo from '@/assets/imgs/svg/logo-nord-art-header.svg'
+import cartIcon from '@/assets/imgs/svg/cart-icon.svg'
+import searchIcon from '@/assets/imgs/svg/search-icon.svg'
+</script>
+
 <template>
-  <div class="flex px-10 py-5 flex-row justify-between">
+  <div class="bg-[#F5F5F5] flex items-center px-[122px] py-[24px] flex-row justify-between">
     <div>
-      Logo
+      <img :src="logo" class="h-10" />
     </div>
-    <nav class="flex flex-row justify-center gap-5">
-      <RouterLink to="/products" class="hover:text-blue-500">Produtos</RouterLink>
-      <RouterLink to="/ctegories" class="hover:text-blue-500">Categorias</RouterLink>
-      <RouterLink to="/about" class="hover:text-blue-500">Sobre</RouterLink>
+    <nav class="flex flex-row justify-center gap-[20px]">
+      <RouterLink to="/products" class="text-[#4f6d3a] hover:text-[#D96C2B]">PRODUTOS</RouterLink>
+      <RouterLink to="/ctegories" class="text-[#4f6d3a] hover:text-[#D96C2B]"
+        >CATEGORIAS</RouterLink
+      >
+      <RouterLink to="/about" class="text-[#4f6d3a] hover:text-[#D96C2B]">SOBRE</RouterLink>
     </nav>
-    <div>
-      Carrinho
+    <div class="flex gap-[20px]">
+      <img :src="searchIcon" />
+      <img :src="cartIcon" />
     </div>
   </div>
 </template>
