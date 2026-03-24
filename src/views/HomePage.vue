48993ead73764f9fea67a0444805b9a6e8d131fc
Gabriel Gurgel <gabrielsilvagurgel@gmail.com>
2026-03-24 11:28:32 -0300

    Adiciona banner e secoes na homepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,48 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import banner1 from '@/assets/imgs/webp/vista_bolsa_palha_macrame_litoral_1.webp'
+</script>
 
 <template>
-  <h1>Code a Home Aqui</h1>
+  <main>
+    <section>
+      <img
+        :src="banner1"
+        alt="Banner com bolsa de palha e macramé da coleção
+      Nord Art"
+        class="w-full"
+      />
+    </section>
+    <section class="bg-palette-cream h-[80vh]">
+      <div>
+        <h2
+          class="text-3xl font-regular text-palette-orange text-center pt-[80px] pb-[20px] uppercase"
+        >
+          Em destaque
+        </h2>
+        <hr class="border-palette-orange w-[50vw] mx-auto" />
+      </div>
+    </section>
+    <section class="bg-palette-gray-light h-[80vh]">
+      <div>
+        <h2
+          class="text-3xl font-regular text-palette-orange text-center pt-[80px] pb-[20px] uppercase"
+        >
+          Categorias
+        </h2>
+        <hr class="border-palette-orange w-[50vw] mx-auto" />
+      </div>
+      <article></article>
+    </section>
+    <section class="bg-palette-gray-light h-[80vh]">
+      <div>
+        <h2
+          class="text-3xl font-regular text-palette-orange text-center pt-[80px] pb-[20px] uppercase"
+        >
+          Produtos
+        </h2>
+        <hr class="border-palette-orange w-[50vw] mx-auto" />
+      </div>
+      <article></article>
+    </section>
+  </main>
 </template>
