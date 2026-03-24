731f41b1dfa0dd6c2fcbc66ef2aeffb14eeb87a0
Gabriel Gurgel <gabrielsilvagurgel@gmail.com>
2026-03-24 19:04:20 -0300

    Adiciona cards a secao de categorias

---
+++ src/views/ButtonAddToCart.vue
@@ -0,0 +1,13 @@
+<template>
+  <button
+    :class="[
+      'cursor-pointer py-3 px-6 rounded-2xl font-semibold text-sm',
+      'border  text-palette-yellow border-palette-yellow bg-none',
+      'hover:text-palette-orange hover:border-palette-orange ',
+      'dark:text-palette-orange dark:border-palette-orange ',
+      'dark:hover:text-palette-yellow dark:hover:border-palette-yellow bg-none',
+    ]"
+  >
+    <slot></slot>
+  </button>
+</template>
